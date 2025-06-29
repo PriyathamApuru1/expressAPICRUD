bdcf04198fb7e3d99c91ee1befdf7e2ba7d0736e
PriyathamApuru1 <priyatham.apuru@gmail.com>
2025-06-29 14:47:30 -0400

    third commit

--- routes/users.js
+++ routes/users.js
@@ -37,6 +37,30 @@ router.get("/:email",(req,res)=>{
   res.send(usersAfterFiltering);
 });
 
+// GET by specific lastname request: Retrieve users with a specific lastname
+router.get("/:lastName",(req,res)=>{
+    const lastName = req.params.lastName;
+    let usersAfterFiltering = users.filter((user) => user.lastName === lastName);
+    res.send(usersAfterFiltering);
+  });
+
+// Function to convert a date string in the format "dd-mm-yyyy" to a Date object
+function getDateFromString(strDate) {
+    let [dd, mm, yyyy] = strDate.split('-');
+    return new Date(yyyy + "/" + mm + "/" + dd);
+}
+
+// Define a route handler for GET requests to the "/sort" endpoint
+router.get("/sort", (req, res) => {
+    // Sort the users array by DOB in ascending order
+    let sorted_users = users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+        return d1 - d2;
+    });
+    // Send the sorted_users array as the response to the client
+    res.send(sorted_users);
+});
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
